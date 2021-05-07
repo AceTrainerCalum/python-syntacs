468520c629ffb4498a00dc6ff23f2f5f9efd2f71
AceTrainerCalum <79257331+AceTrainerCalum@users.noreply.github.com>
2021-05-07 20:06:14 +0100

    Add files via upload
    this is the main file if you read the code you will get an example for the basic syntacs in python

---
+++ main.py
@@ -0,0 +1,40 @@
+'''
+this will be a python syntacs
+your basic syntacs are:
+
+print
+input
+strings (str)
+integers (int)
+float
+'''
+
+print("hello world!")
+
+#first and most basic command
+
+input("this is an imput command right?")
+
+#this is a basic input command, this can be used to data to a 'variable' like this
+
+data = input("this responce will be recored in a variable called data")
+
+#this will be recored in a variable called data
+
+#data can also be recored with out the input like this:
+
+name = ("ace")
+
+'''
+the next 3 things string integers and floats
+
+strings are letters only
+integer and floats are nummbers only
+'''
+
+string = str("this non-nummber statment is a string")
+integer = int("123")
+float1 = float(integer)
+
+#an integer can only be nummbers and a float can only be a variable
+
